d2bf9d77e2264d59ed092a5a66e94266b082ff3c
Chris Forrest <redrider_cr@yahoo.com>
2020-06-19 17:02:13 -0400

    initial push added dependencies set up initialState in addFeature reducer and set up store and provider in index.js set up state in app.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+/**********************my imports for redux and initial state *************************/
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { addFeature } from './reducers/addFeature';
 
 import 'bulma/css/bulma.css';
 import './styles.scss';
 
+/**********************create a redux store  ***************************************/
+const store = createStore(addFeature);
+
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(<Provider store={store}>
+    <App />
+    </Provider>, 
+    rootElement);
